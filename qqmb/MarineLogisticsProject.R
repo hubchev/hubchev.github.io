4642b7ecfb5fbf09daa48e3482ee2b604b434e99
hubchev <DrStephanHuber@yahoo.com>
2023-03-02 12:48:00 +0100

    praesi

---
+++ qqmb/MarineLogisticsProject.R
@@ -0,0 +1,28 @@
+## ----setup, include=FALSE-----------------------------------------------------
+library("knitr")
+knit_hooks$set(purl = hook_purl)
+
+## ----include=FALSE, echo=FALSE------------------------------------------------
+getwd()
+
+## ----echo=FALSE, out.width="40%", fig.width=6, fig.height=4-------------------
+knitr::include_graphics("Container_Vessel.jpeg", error = FALSE)
+
+## ----echo=FALSE, out.width="40%", fig.width=6, fig.height=4-------------------
+knitr::include_graphics("Bulk_Carrier.png", error = FALSE)
+
+## ----echo=FALSE, out.width="40%", fig.width=6, fig.height=4-------------------
+knitr::include_graphics("Tanker_Vessel.png", error = FALSE)
+
+## ----echo=FALSE, out.width="40%", fig.width=6, fig.height=4-------------------
+knitr::include_graphics("Ro-Ro.jpeg", error = FALSE)
+
+## ----echo=FALSE, out.width="40%", fig.width=6, fig.height=4-------------------
+knitr::include_graphics("Passenger.jpeg", error = FALSE)
+
+## ----echo=FALSE, out.width="40%", fig.width=6, fig.height=4-------------------
+knitr::include_graphics("offshore-vessel.jpeg", error = FALSE)
+
+## ----echo=FALSE, out.width="40%", fig.width=6, fig.height=4-------------------
+knitr::include_graphics("Fishing_Vessel.jpeg", error = FALSE)
+
